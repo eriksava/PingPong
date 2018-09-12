94d5477386b3e22943b8a7227eb8b6622734a4a0
Erik Sava <eriksava@ErikSava-Macbook-Pro.local>
2018-09-11 19:23:23 -0700

    game scene created

---
+++ Assets/Scripts/PlayBotton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PlayBotton : MonoBehaviour {
+
+    public void PlayGame(){
+
+        SceneManager.LoadScene("Game");
+
+
+
+    } 
+
+}
